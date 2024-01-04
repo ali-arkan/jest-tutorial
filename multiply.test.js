cae8b929f4b11da1c18f7cb4a8bb972b419e7cae
ali-arkan <alidark.sf@gmail.com>
2024-01-04 10:19:37 +0300

    multiply has been modified

--- multiply.test.js
+++ multiply.test.js
@@ -1,11 +1,26 @@
 const multiply = require('./multiply');
 
 describe('test multiply positive scenarios',() =>{
-    test('multiply 3*2 should equal to 6',() =>{
-        expect(multiply(3,2)).toBe(6);
-    });
-})
 
+// Success toBe
+test('multiply 3*2 should equal to 6',() =>{
+    expect(multiply(3,2)).toBe(6);
+    });
+
+
+// Error toBe
+test('multiply 4*3 should not equal to 10',() =>{
+    expect(multiply(4,3)).toBe(10);
+});
+
+
+// Success for not.toBe
+test('multiply 4*3 should  not equal to 10',() =>{
+    expect(multiply(4,3)).not.toBe(10);
+});
+
+
+//toEqual
 test('object example',()=>
 {
     const data = {first: 1};
@@ -13,6 +28,4 @@ test('object example',()=>
     expect(data).toEqual({first:1, second: 2});
 });
 
-test('multiply 4*3 should  not equal to 11',() =>{
-    expect(multiply(4,3)).not.toBe(11);
-});
+})
